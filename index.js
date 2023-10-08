fd6b7b6471681052974db77b69fd74a9b801ad99
Akash Singh <mr.singhaksh@gmail.com>
2023-10-09 00:42:26 +0530

    setup:Initial server setup

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import kpiRoutes from "./routes/kpi";
+
+// CONFIGURATION
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROUTES
+
+app.use("/kpi", kpiRoutes);
+
+// MONGOOSE SETUP
+
+const PORT = process.env.PORT || 9000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    newUserUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port ${PORT}`));
+  })
+  .catch(err => console.log(`${err} did not connect`));
