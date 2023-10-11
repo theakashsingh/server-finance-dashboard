9787cde6312efadec2f0f25d58a672ae1a170ee7
Akash Singh <mr.singhaksh@gmail.com>
2023-10-11 19:35:00 +0530

    feat:Create transaction API

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
 import productRoutes from "./routes/product.js"
+import transactionRoutes from "./routes/transaction.js"
 import Product from "./models/Product.js"
 import KPI from "./models/KPI.js";
-import {kpis, products} from "./data/data.js"
+import Transaction from "./models/Transaction.js"
+import {kpis, products, transactions} from "./data/data.js"
 
 // CONFIGURATION
 
@@ -28,6 +30,7 @@ app.use(cors());
 
 app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
 // MONGOOSE SETUP
 
@@ -45,5 +48,6 @@ mongoose
     // await mongoose.connection.db.dropDatabase()
     // KPI.insertMany(kpis)
     // Product.insertMany(products)
+    // Transaction.insertMany(transactions)
   })
   .catch(err => console.log(`${err} did not connect`));
